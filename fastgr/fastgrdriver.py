e0b7d754be1136b5ae5f195bbb68c8d4f55b4d0a
Wenduo Zhou <zhouw@ornl.gov>
2016-06-08 00:22:11 -0400

    Checkpointing progress of implementing tab G(r).

---
+++ fastgr/fastgrdriver.py
@@ -0,0 +1,158 @@
+import os
+import numpy as np
+import mantidapi as mtd
+from mantid.kernel import AnalysisDataService
+
+class FastGRDriver(object):
+    """
+    Driver for FastGR application
+    """
+    def __init__(self):
+        """ Initialization
+        Returns
+        -------
+
+        """
+        # name of the MatrixWorkspace for S(Q)
+        self._currSqWsName = ''
+
+        # dictionary of the workspace with rmin, rmax and delta r setup
+        self._grWsIndex = 0
+        self._grWsNameDict = dict()
+
+        return
+
+    def calculate_gr(self, min_r, delta_r, max_r, min_q, max_q):
+        """
+        Calculate G(R)
+        Parameters
+        ----------
+        min_r :: R_min
+        delta_r :: delta R
+        max_r
+        min_q
+        max_q
+
+        Returns
+        -------
+        None
+        """
+        # check
+        assert min_r < max_r, 'Rmin must be less than Rmax (%f >= %f)' % (min_r, max_r)
+        assert delta_r < (max_r - min_r), 'Must have more than one bin in G(r) (%f >= %f)' \
+                                     '' % (delta_r, (max_r - min_r))
+
+        assert min_q < max_q, 'Qmin must be less than Qmax (%f >= %f)' % (min_q, max_q)
+
+        # set up the parameters for FourierTransform
+        # output workspace
+        gr_ws_name = 'G(R)_%s' % self._currSqWsName
+        kwargs = {'OutputWorkspace': gr_ws_name,
+                  'Qmin': min_q,
+                  'Qmax': max_q,
+                  'PDFType': 'G(r)',
+                  'DeltaR': delta_r,
+                  'Rmax': max_r}
+
+        # get the input unit
+        sq_ws = AnalysisDataService.retrieve(self._currSqWsName)
+        inputsofqtype = sq_ws.YUnitLabel()
+
+        # do the FFT
+        mtd.PDFFourierTransform(InputWorkspace=self._currSqWsName,
+                                InputSofQType=inputsofqtype,
+                                **kwargs)
+
+        # check
+        assert AnalysisDataService.doesExist(gr_ws_name), 'Failed to do Fourier Transform.'
+        self._grWsNameDict[(min_r, max_r, delta_r)] = gr_ws_name
+
+        return
+
+    def get_gr(self, min_r, delta_r, max_r):
+        """ Get G(r)
+        Parameters
+        ----------
+        min_r
+        delta_r
+        max_r
+
+        Returns
+        -------
+        3-tuple for numpy.array
+        """
+        # check... find key in dictionary
+        error_msg = 'R-range and delta R are not support. Current stored G(R) parameters are %s.' \
+                    '' % str(self._grWsNameDict.keys())
+        assert (min_r, max_r, delta_r) in self._grWsNameDict, error_msg
+
+        # get the workspace
+        gr_ws = AnalysisDataService.retrieve((min_r, max_r, delta_r))
+
+        return gr_ws.readX(0), gr_ws.readY(0), gr_ws.readE(0)
+
+    def get_sq(self):
+        """
+        Get S(Q)
+        Returns
+        -------
+        3-tuple of numpy array as Q, S(Q) and Sigma(Q)
+        """
+        assert AnalysisDataService.doesExist(self._currSqWsName), 'S(Q) matrix workspace %s does not exist.' \
+                                                                  '' % self._currSqWsName
+
+        out_ws = AnalysisDataService.retrieve(self._currSqWsName)
+
+        return out_ws.readX(0), out_ws.readY(0), out_ws.readE(0)
+
+    def load_bragg_file(self, file_name):
+        """
+        Load Bragg diffraction file (including 3-column data file, GSAS file) for Rietveld
+        Parameters
+        ----------
+        file_name
+
+        Returns
+        -------
+
+        """
+        # load with different file type
+        base_file_name = os.path.basename(file_name).lower()
+        gss_ws_name = os.path.basename(file_name).split('.')[0]
+        if base_file_name.endswith('.gss') or base_file_name.endswith('.gsa'):
+            mtd.LoadGSS(Filename=file_name,
+                        OutputWorkspace=gss_ws_name)
+        elif base_file_name.endswith('.dat'):
+            mtd.LoadAscii(Filename=file_name,
+                          OutputWorkspace=gss_ws_name,
+                          Unit='TOF')
+        else:
+            raise RuntimeError('File %s is not of a supported type.' % file_name)
+
+        # check
+        assert
+
+
+    def load_sq(self, file_name):
+        """
+        Load S(Q) to a numpy
+        Guarantees: the file is loaded to self._currSQX, _currSQY and _currSQE
+        Parameters
+        ----------
+        file_name :: name of the S(Q)
+
+        Returns
+        -------
+        2-tuple as load-status, error-mesage
+        """
+        out_ws_name = os.path.basename(file_name).split('.')[0]
+
+        mtd.LoadAscii(Filename=file_name,
+                      OutputWorkspace=out_ws_name,
+                      Unit='MomentumTransfer')
+        assert AnalysisDataService.doesExist(out_ws_name), 'Unable to load S(Q) file %s.' % file_name
+
+        self._currSqWsName = out_ws_name
+
+        return
+
--- fastgr/launcher.py
+++ fastgr/launcher.py
@@ -1,15 +1,19 @@
 import sys
 
-
 import ui_mainWindow
 
+import PyQt4
 import PyQt4.QtCore as QtCore
-import PyQt4.QtGui
+import PyQt4.QtGui as QtGui
+
+import fastgrdriver as driver
 
 __version__ = "1.0.0"
 
 
 class MainWindow(PyQt4.QtGui.QMainWindow):
+    """ Main FastGR window
+    """
 
     def __init__(self, parent=None):
         super(PyQt4.QtGui.QMainWindow, self).__init__(parent)
@@ -41,22 +45,23 @@ class MainWindow(PyQt4.QtGui.QMainWindow):
                      self.evt_plot_bragg_bank)
 
         # for tab G(R)
-        """
         self.connect(self.ui.pushButton_loadSQ, QtCore.SIGNAL('clicked()'),
                      self.do_load_sq)
         self.connect(self.ui.radioButton_sq, QtCore.SIGNAL('toggled(int)'),
                      self.evt_plot_sq)
         self.connect(self.ui.radioButton_sq, QtCore.SIGNAL('toggled(int)'),
-                     self.evt_plot_sqm1)
+                     self.evt_plot_sq)
         self.connect(self.ui.radioButton_sq, QtCore.SIGNAL('toggled(int)'),
-                     self.evt_plot_qsqm1)
+                     self.evt_plot_sq)
         self.connect(self.ui.pushButton_showQMinMax, QtCore.SIGNAL('clicked()'),
                      self.do_show_sq_bound)
         self.connect(self.ui.pushButton_generateGR, QtCore.SIGNAL('clicked()'),
                      self.do_generateGR)
         self.connect(self.ui.pushButton_saveGR, QtCore.SIGNAL('clicked()'),
                      self.do_save_GR)
-        """
+
+        # define the driver
+        self._myController = driver.FastGRDriver()
 
         return
 
@@ -75,6 +80,16 @@ class MainWindow(PyQt4.QtGui.QMainWindow):
         Returns
         -------
         """
+        # get file
+        ext = 'GSAS (*.gsa, *.gss);;DAT (*.dat);;Nexus (*.nxs)'
+        bragg_file_name = str(QtGui.QFileDialog.getOpenFileName(self, 'Choose Bragg File', ext))
+        if bragg_file_name is None:
+            return
+
+        # load file
+        self._myController.load_bragg_file(bragg_file_name)
+
+        #
 
         return
 
@@ -85,6 +100,34 @@ class MainWindow(PyQt4.QtGui.QMainWindow):
         -------
 
         """
+        # get the file
+        ext = 'DAT (*.dat);;All (*.*)'
+        sq_file_name = str(QtGui.QFileDialog.getOpenFileName(self, 'Choose S(Q) File', ext))
+        if sq_file_name is None:
+            return
+
+        # open the file
+        self._myController.load_sq(sq_file_name)
+
+        # plot the figure
+        vec_q, vec_s, vec_e = self._myController.get_sq()
+        self.ui.graphicsView_sq.plot_sq(vec_q, vec_s, vec_e)
+        self.ui.radioButton_sq.setChecked(True)
+
+        # calculate the G(R)
+        min_r = float(self.ui.doubleSpinBoxQmin.value())
+        max_r = float(self.ui.doubleSpinBoxQmax.value())
+        delta_r = float(self.ui.doubleSpinBoxDelR.value())
+
+        min_q = float(self.doubleSpinBoxQmin.value())
+        max_q = float(self.doubleSpinBoxQmax.value())
+
+        self._myController.calculate_gr(min_r, delta_r, max_r, min_q, max_q)
+
+        # plot G(R)
+        vec_r, vec_g, vec_ge = self._myController.get_gr(min_r, delta_r, max_r)
+        self.ui.graphicsView_gr.plot_gr(vec_r, vec_g, vec_ge)
+
         return
 
     def evt_plot_bragg_bank(self):
@@ -94,6 +137,39 @@ class MainWindow(PyQt4.QtGui.QMainWindow):
         -------
 
         """
+
+        return
+
+    def evt_plot_sq(self):
+        """ Event handling to plot S(Q)
+        Returns
+        -------
+
+        """
+        # get the raw S(Q)
+        vec_q, vec_sq, vec_se = self._myController.get_sq()
+
+        # get the unit
+        sq_unit = None
+        if self.ui.radioButton_sq.isChecked():
+            # use the original S(Q)
+            sq_unit = 'S(Q)'
+            vec_y = vec_sq
+        elif self.ui.radioButton_sqm1.isChecked():
+            # use S(Q)-1
+            sq_unit = 'S(Q)-1'
+            vec_y = vec_sq - 1
+        elif self.ui.radioButton_qsqm1:
+            # use Q(S(Q)-1)
+            sq_unit = 'Q(S(Q)-1)'
+            vec_y = vec_q * (vec_sq - 1)
+        else:
+            raise RuntimeError('None of S(Q), S(Q)-1 or Q(S(Q)-1) is chosen.')
+
+        # plot
+        self.ui.graphicsView_gr.plot_gr(vec_q, vec_y, vec_se, sq_unit)
+
+        return
 
 
 def main():
