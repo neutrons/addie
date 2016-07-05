aebd859b3796a7b77330419f987b9239a73b99a8
Jean Bilheux <bilheuxjm@ornl.gov>
2016-07-05 10:41:31 -0400

    Started to implement run_sum_scans

--- fastgr/step2_handler/run_ndabs.py
+++ fastgr/step2_handler/run_ndabs.py
@@ -15,5 +15,5 @@ class RunNDabs(object):
     def run(self):
         _str_list_sample_files = " ".join(self.list_sample_files)
         print("[LOG] executing:")
-        print("[LOG] " + self.script_to_run + ' ' + _str_list_sample_files)
+        print("[LOG] " + self.script_to_run + ' ' + _str_list_sample_files + " &")
         os.system(self.script_to_run + ' ' + _str_list_sample_files)
--- fastgr/step2_handler/run_sum_scans.py
+++ fastgr/step2_handler/run_sum_scans.py
@@ -1,5 +1,7 @@
 class RunSumScans(object):
     
+    script = 'python  /SNS/users/zjn/pytest/sumscans.py '
+    
     def __init__(self, parent=None):
         self.parent = parent
         
