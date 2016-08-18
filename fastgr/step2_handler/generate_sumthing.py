12e0d70ccddb784d66ff0fcb7ffa8c4251cd0afe
Jean Bilheux <bilheuxjm@ornl.gov>
2016-08-18 10:12:10 -0400

    program is now able to load new format with no scan info in it

--- fastgr/step2_handler/generate_sumthing.py
+++ fastgr/step2_handler/generate_sumthing.py
@@ -9,30 +9,57 @@ reads in a los.txt file, writes out auto_sum.inp file
 from collections import defaultdict
 import sys
 import os.path
+from fastgr.utilities.file_handler import FileHandler
+
 
 class GenerateSumthing(object):
 
     input_file_old_format = 'los.txt'
     input_file_new_format = 'los.csv'
+    output_inp_file = 'auto_sum.inp'
     
     def __init__(self, folder=None):
         self.folder = folder
         
     def create_sum_inp_file(self):
         
-        # try new output first
+        # try new format (csv) with comma separated and scan column
         full_input_file_name_new_format = os.path.join(self.folder, self.input_file_new_format)
         if os.path.isfile(full_input_file_name_new_format):
             self.create_sum_inp_file_from_new_format(full_input_file_name_new_format)
+            if self.is_sum_inp_file_not_empty():
+                return
+        
+        # try new format (csv) with comma separated but no scan column
+        if os.path.isfile(full_input_file_name_new_format):
+            self.create_sum_inp_file_from_new_format_2(full_input_file_name_new_format)
+            if self.is_sum_inp_file_not_empty():
+                return
+
+        # try old format with space separated columns
+        full_input_file_name_old_format = os.path.join(self.folder, self.input_file_old_format)
+        if os.path.isfile(full_input_file_name_old_format):
+            self.create_sum_inp_file_from_old_format(full_input_file_name_old_format)
+            if not self.is_sum_inp_file_not_empty():
+                raise ValueError("Format of file not compatible!")
+            
         else:
-            full_input_file_name_old_format = os.path.join(self.folder, self.input_file_old_format)
-            if os.path.isfile(full_input_file_name_old_format):
-                self.create_sum_inp_file_from_old_format(full_input_file_name_old_format)
-        #else:
-        #        raise IOError("lost.* file does not exist!")
+            raise IOError("lost.* file does not exist!")
+
+    def is_sum_inp_file_not_empty(self):
+        full_output_file_name = os.path.join(self.folder, self.output_inp_file)
+        _o_file = FileHandler(filename = full_output_file_name)
+        _o_file.retrieve_contain()
+        _file_contain = _o_file.file_contain
+        if len(_file_contain) == 12:
+            return False
+        return True        
 
     def create_sum_inp_file_from_new_format(self,  full_input_file_name):
-        
+        '''
+        Comma separated with scan column
+        '''
+        print("]LOG]  Format: comma separated, with scan infos")
         print("[LOG] Reading %s" %full_input_file_name)
         name_list = []
         run_nums = defaultdict(list)
@@ -60,7 +87,7 @@ class GenerateSumthing(object):
                     name_list.append(word)
                     run_nums[word].append(run_num)
     
-        full_output_file_name = os.path.join(self.folder, "auto_sum.inp")        
+        full_output_file_name = os.path.join(self.folder, self.output_inp_file)
     
         outfile = open(full_output_file_name, "w")
         outfile.write("background \n")
@@ -76,8 +103,55 @@ class GenerateSumthing(object):
         outfile.close()
         print("[LOG] Created %s" %full_output_file_name)
 
+    def create_sum_inp_file_from_new_format_2(self,  full_input_file_name):
+        
+        print("]LOG]  Format: comma separated, no scan infos")
+        print("[LOG] Reading %s" %full_input_file_name)
+        name_list = []
+        run_nums = defaultdict(list)
+        
+        with open(full_input_file_name, "r") as myfile:
+            data = myfile.readlines()
+                  
+        data = data[1:]
+        for _row in data:
+            _row_split = _row.split(',')
+            
+            word = _row_split[-1].strip()
+            word = word.replace(":", "_")
+            word = word.replace(" ", "_")
+            
+            run_num = int(_row_split[0])
+            
+            run_nums[word].append(run_num)
+            if not (word in name_list):
+                name_list.append(word)
+    
+        full_output_file_name = os.path.join(self.folder, self.output_inp_file)
+    
+        outfile = open(full_output_file_name, "w")
+        outfile.write("background \n")
+    
+        print("run_nums")
+        print(run_nums)
+        
+        print('name_list')
+        print(name_list)
+    
+        #print(">creating file %s" %full_output_file_name)
+        for key in sorted(run_nums.iterkeys()):
+            outbit = str(run_nums[key])
+            outbit = outbit.replace("[","")
+            outbit = outbit.replace("]","")
+            outbit = outbit.replace(" ","")
+            outfile.write(key + " " + outbit+"\n")
+    
+        outfile.close()
+        print("[LOG] Created %s" %full_output_file_name)
+
     def create_sum_inp_file_from_old_format(self, full_input_file_name):
 
+        print("]LOG]  Format: space separated, with scan infos")
         print("[LOG] Reading %s" %full_input_file_name)
         name_list=[]
         run_nums=defaultdict(list)
@@ -101,7 +175,7 @@ class GenerateSumthing(object):
                         name_list.append(word)
                         run_nums[word].append(run_num)
         
-        full_output_file_name = os.path.join(self.folder, "auto_sum.inp")        
+        full_output_file_name = os.path.join(self.folder, self.output_inp_file)
         
         outfile = open(full_output_file_name, "w")
         outfile.write("background \n")
