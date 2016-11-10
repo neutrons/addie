a129c90d1fa5198422292b8f598841f62c9463a1
Wenduo Zhou <zhouw@ornl.gov>
2016-11-10 10:49:08 -0500

    Revert "Implement legend control for all figures"

--- fastgr/ipythondockwidget.py
+++ fastgr/ipythondockwidget.py
@@ -22,3 +22,35 @@ class IPythonDockWidget(PyQt4.QtGui.QDockWidget):
         self.setWidget(self.iPythonWidget)
 
         return
+
+    def wild_test(self):
+        """
+        WILD TEST
+        Returns
+        -------
+
+        """
+        # append output to the output stream but not input console
+        self.iPythonWidget.append_stream('bababa (append_stream)')
+
+        import os
+        file_name = os.path.join(os.getcwd(), 'blabla.py')
+        self.iPythonWidget.execute_file(file_name)
+        # result:
+        # %run /home/wzz/Projects/FastGR/test_data/blabla.py
+        # Hello World!
+
+        self.iPythonWidget.input_buffer = 'print (nothing)'
+
+        return
+        # append to output
+        self.iPythonWidget._append_plain_text('22222')
+
+        # not inherit from HistoryConsoleWidget
+        # pre_hist = self.iPythonWidget.history_previous()
+        # print 'pre_hist = ', pre_hist
+
+        command = 'ws = mtd[abc]'
+        self.iPythonWidget.write_command(command)
+
+        return
