39a4349a3c961a8cbfde0d34d306ea49df03b5db
Wenduo Zhou <zhouw@ornl.gov>
2018-02-22 11:46:16 -0500

    Fixed save GofR and SofQ issues.

--- addie/mantid_ipython_widget.py
+++ addie/mantid_ipython_widget.py
@@ -16,10 +16,13 @@ from pygments.lexer import RegexLexer
 # Monkeypatch!
 RegexLexer.get_tokens_unprocessed_unpatched = RegexLexer.get_tokens_unprocessed
 
-#from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
-from qtconsole.rich_ipython_widget import RichIPythonWidget
-#from IPython.qt.inprocess import QtInProcessKernelManager
-from qtconsole.inprocess import QtInProcessKernelManager
+wz_linux = True
+if wz_linux:
+    from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
+    from IPython.qt.inprocess import QtInProcessKernelManager
+else:
+    from qtconsole.rich_ipython_widget import RichIPythonWidget
+    from qtconsole.inprocess import QtInProcessKernelManager
 
 from mantid.api import AnalysisDataService as mtd
 
