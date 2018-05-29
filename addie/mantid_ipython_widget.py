b3d30fba4731db538273688afd15eda3de7322ef
McDonnell, Marshall T <mcdonnellmt@ornl.gov>
2018-05-29 11:56:13 -0400

    Removed hard-coded Mantid path

--- addie/mantid_ipython_widget.py
+++ addie/mantid_ipython_widget.py
@@ -4,11 +4,6 @@ import inspect
 import sys
 import os
 
-sys.path.append('/opt/mantid38/bin/')
-# sys.path.append('/opt/mantidnightly/bin/')
-# sys.path.append(os.path.join(os.path.expanduser('~/MantidBuild/debug/bin/')))
-sys.path.append('/Users/wzz/MantidBuild/debug-stable/bin/')
-
 # IPython monkey patches the  pygments.lexer.RegexLexer.get_tokens_unprocessed method
 # and breaks Sphinx when running within MantidPlot.
 # We store the original method definition here on the pygments module before importing IPython
