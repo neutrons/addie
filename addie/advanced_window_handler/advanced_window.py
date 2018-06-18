42bea66e72c33dc22a8e7bb34f1f9a20e2091d4c
Jean Bilheux <bilheuxjm@ornl.gov>
2018-06-18 12:56:27 -0400

    Status of idl or mantid post processing is now saved

--- addie/advanced_window_handler/advanced_window.py
+++ addie/advanced_window_handler/advanced_window.py
@@ -13,11 +13,26 @@ class AdvancedWindow(QtGui.QMainWindow):
         self.ui.setupUi(self)
         
         self.setWindowTitle("Advanced Window for Super User Only !")
-        
+        self.init_widgets()
+
+    def init_widgets(self):
+        _idl_status = False
+        _mantid_status = False
+        if self.parent.post_processing == 'idl':
+            _idl_status = True
+        else:
+            _mantid_status = True
+
+        self.ui.idl_post_processing_button.setChecked(_idl_status)
+        self.ui.mantid_post_processing_button.setChecked(_mantid_status)
+
     def post_processing_clicked(self):
         if self.ui.idl_post_processing_button.isChecked():
             _index = 0
+            _post = 'idl'
         else:
             _index = 1
+            _post = 'mantid'
 
         self.parent.ui.stackedWidget.setCurrentIndex(_index)
+        self.parent.post_processing = _post
