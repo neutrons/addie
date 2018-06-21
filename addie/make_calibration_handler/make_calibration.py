c5788f9fe18af5a791a04c19b864400c1f01dcdf
Jean Bilheux <bilheuxjm@ornl.gov>
2018-06-21 16:49:50 -0400

    Improving make calculation ui to allow connection of browse buttons

--- addie/make_calibration_handler/make_calibration.py
+++ addie/make_calibration_handler/make_calibration.py
@@ -22,6 +22,7 @@ class MakeCalibrationWindow(QtGui.QMainWindow):
 
     table_column_width = [250, 250, 80, 300]
     table_row_height = 40
+    entry_level =  0
 
     def __init__(self, parent=None):
         self.parent = parent
@@ -51,27 +52,25 @@ class MakeCalibrationWindow(QtGui.QMainWindow):
         row_selected = o_gui.get_current_row()
         self.__insert_new_row(row=row_selected+1)
 
+    def vanadium_browse_clicked(self, row=-1):
+        print(row)
+
+    def calibration_browse_clicked(self):
+        pass
+
     def __insert_new_row(self, row=-1):
         self.ui.tableWidget.insertRow(row)
         self.ui.tableWidget.setRowHeight(row, self.table_row_height)
 
         button_width = 80
 
-        #column 0 - calibration
-        label = QtGui.QLabel("Run #:")
-        value = QtGui.QLineEdit("")
-        button = QtGui.QPushButton("Browse ...")
-        button.setMinimumWidth(button_width)
-        button.setMaximumWidth(button_width)
-        hori_layout = QtGui.QHBoxLayout()
-        hori_layout.addWidget(label)
-        hori_layout.addWidget(value)
-        hori_layout.addWidget(button)
-        col1_widget = QtGui.QWidget()
-        col1_widget.setLayout(hori_layout)
-        self.ui.tableWidget.setCellWidget(row, 0, col1_widget)
+        #column0 - entry
+        _name = str(self.entry_level+1)
+        self.entry_level += 1
+        label = QtGui.QLabel(_name)
+        self.ui.tableWidget.setCellWidget(label)
 
-        #column 1 - Vanadium
+        #column 1 - calibration
         label = QtGui.QLabel("Run #:")
         value = QtGui.QLineEdit("")
         button = QtGui.QPushButton("Browse ...")
@@ -85,11 +84,26 @@ class MakeCalibrationWindow(QtGui.QMainWindow):
         col1_widget.setLayout(hori_layout)
         self.ui.tableWidget.setCellWidget(row, 1, col1_widget)
 
-        #column 2 - date
-        date = QtGui.QDateEdit()
-        self.ui.tableWidget.setCellWidget(row, 2, date)
+        #column 2 - Vanadium
+        label = QtGui.QLabel("Run #:")
+        value = QtGui.QLineEdit("")
+        button = QtGui.QPushButton("Browse ...")
+        button.setMinimumWidth(button_width)
+        button.setMaximumWidth(button_width)
+        button.clicked.connect(lambda state, row=row: self.vanadium_browse_clicked(row))
+        hori_layout = QtGui.QHBoxLayout()
+        hori_layout.addWidget(label)
+        hori_layout.addWidget(value)
+        hori_layout.addWidget(button)
+        col1_widget = QtGui.QWidget()
+        col1_widget.setLayout(hori_layout)
+        self.ui.tableWidget.setCellWidget(row, 2, col1_widget)
 
-        #column 3 - output dir
+        #column 3 - date
+        date = QtGui.QDateEdit()
+        self.ui.tableWidget.setCellWidget(row, 3, date)
+
+        #column 4 - output dir
         label = QtGui.QLabel("N/A")
         button = QtGui.QPushButton("Browse...")
         button.setMinimumWidth(button_width)
@@ -99,10 +113,7 @@ class MakeCalibrationWindow(QtGui.QMainWindow):
         hori_layout.addWidget(button)
         col3_widget = QtGui.QWidget()
         col3_widget.setLayout(hori_layout)
-        self.ui.tableWidget.setCellWidget(row, 3, col3_widget)
-
-
-
+        self.ui.tableWidget.setCellWidget(row, 4, col3_widget)
 
     def run_calibration_button_clicked(self):
         print("run calibration")
