a15a6797cd0199747c4dd9ace64fba1a6482e0a7
JeanBilheux <bilheuxjm@ornl.gov>
2018-06-22 10:33:44 -0400

    Built ui

--- addie/ui_make_calibration.py
+++ addie/ui_make_calibration.py
@@ -25,7 +25,7 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(1025, 541)
+        MainWindow.resize(1619, 515)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.centralwidget)
@@ -121,7 +121,7 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.addLayout(self.horizontalLayout_4)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1025, 20))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1619, 20))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
