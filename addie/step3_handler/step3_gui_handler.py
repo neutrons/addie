9c6a77f50047e8cc66b1b3f376deaee3a84082a5
Pete Peterson <petersonpf@ornl.gov>
2018-11-16 13:15:39 -0500

    Convert from pyqt4 to qtpy

--- addie/step3_handler/step3_gui_handler.py
+++ addie/step3_handler/step3_gui_handler.py
@@ -1,4 +1,4 @@
-from PyQt4 import QtGui, QtCore
+from qtpy.QtWidgets import (QFileDialog)
 import os
 
 from addie.utilities.file_handler import FileHandler
@@ -11,17 +11,20 @@ class Step3GuiHandler(object):
         self.parent_no_ui = parent
         self.parent = parent.ui
         self.current_folder = parent.current_folder
-        
+
     def browse_file(self):
         _current_folder = self.current_folder
-        _ascii_file = QtGui.QFileDialog.getOpenFileName(parent = self.parent_no_ui,
+        _ascii_file = QFileDialog.getOpenFileName(parent = self.parent_no_ui,
                                                              caption = 'Select file to display',
                                                              directory = self.current_folder)
-        
-	if str(_ascii_file):
-	    o_file_handler = FileHandler(filename = _ascii_file)
-	    o_file_handler.retrieve_contain()
-	    text_contain = o_file_handler.file_contain
-	    
-	    o_preview = PreviewAsciiWindow(parent = self.parent_no_ui, text = text_contain, filename=_ascii_file)
-	    o_preview.show()
+        if not _ascii_file:
+            return
+        if isinstance(_ascii_file, tuple):
+            _ascii_file = _ascii_file[0]
+        _ascii_file = str(_ascii_file)
+        o_file_handler = FileHandler(filename = _ascii_file)
+        o_file_handler.retrieve_contain()
+        text_contain = o_file_handler.file_contain
+
+        o_preview = PreviewAsciiWindow(parent = self.parent_no_ui, text = text_contain, filename=_ascii_file)
+        o_preview.show()
