ec3a353ae92a377f907b0d9f45cd034f5731e9d2
JeanBilheux <bilheuxjm@ornl.gov>
2018-12-05 10:30:28 -0500

    reduction configuration now saves the new values of the widgets (some for now) when exiting the window and restoring them when coming back to it

--- addie/master_table/reduction_configuration_handler.py
+++ addie/master_table/reduction_configuration_handler.py
@@ -46,16 +46,22 @@ class ReductionConfiguration(QDialog):
         self.ui.reset_pdf_r_range_button.setIcon(QtGui.QIcon(":/MPL Toolbar/reset_logo.png"))
 
         # list of sample environment
-        config_file = self.parent.addie_config_file
-        with open(config_file) as f:
-            data = json.load(f)
-        pdf_q_range = data['pdf_q_range']
+        if self.parent.reduction_configuration == {}:
+            config_file = self.parent.addie_config_file
+            with open(config_file) as f:
+                data = json.load(f)
+            pdf_q_range = data['pdf_q_range']
+            pdf_r_range = data['pdf_r_range']
+
+        else:
+
+            pdf_q_range = self.parent.reduction_configuration['pdf_q_range']
+            pdf_r_range = self.parent.reduction_configuration['pdf_r_range']
 
         self.ui.pdf_q_range_min.setText(str(pdf_q_range["min"]))
         self.ui.pdf_q_range_max.setText(str(pdf_q_range["max"]))
         self.ui.pdf_q_range_delta.setText(str(pdf_q_range["delta"]))
 
-        pdf_r_range = data['pdf_r_range']
         self.ui.pdf_r_range_min.setText(str(pdf_r_range["min"]))
         self.ui.pdf_r_range_max.setText(str(pdf_r_range["max"]))
         self.ui.pdf_r_range_delta.setText(str(pdf_r_range["delta"]))
@@ -71,13 +77,42 @@ class ReductionConfiguration(QDialog):
 
     def close_button(self):
         # save state of buttons
+        SaveReductionConfiguration(parent=self, grand_parent=self.parent)
 
         # close
-        self.closeEvent(event=None)
+        self.close()
 
     def closeEvent(self, event=None):
         self.parent.reduction_configuration_ui = None
         self.parent.reduction_configuration_ui_position = self.pos()
+
+
+class SaveReductionConfiguration:
+
+    def __init__(self, parent=None, grand_parent=None):
+        reduction_configuration = {}
+
+        pdf_q_range_min = self._get_text_value(parent.ui.pdf_q_range_min)
+        pdf_q_range_max = self._get_text_value(parent.ui.pdf_q_range_max)
+        pdf_q_range_delta = self._get_text_value(parent.ui.pdf_q_range_delta)
+        reduction_configuration['pdf_q_range'] = {'min': pdf_q_range_min,
+                                                  'max': pdf_q_range_max,
+                                                  'delta': pdf_q_range_delta}
+
+        pdf_r_range_min = self._get_text_value(parent.ui.pdf_r_range_min)
+        pdf_r_range_max = self._get_text_value(parent.ui.pdf_r_range_max)
+        pdf_r_range_delta = self._get_text_value(parent.ui.pdf_r_range_delta)
+        reduction_configuration['pdf_r_range'] = {'min': pdf_r_range_min,
+                                                  'max': pdf_r_range_max,
+                                                  'delta': pdf_r_range_delta}
+
+        grand_parent.reduction_configuration = reduction_configuration
+
+    def _get_text_value(self, ui=None):
+        if ui is None:
+            return ""
+
+        return str(ui.text())
 
 
 
