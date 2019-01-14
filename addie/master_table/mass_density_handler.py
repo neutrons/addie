54918dc39b3bb3ec003d4c6d4328902173f09697
JeanBilheux <bilheuxjm@ornl.gov>
2019-01-14 15:20:20 -0500

    mass density handler is in place. Ready to be fully implemented

---
+++ addie/master_table/mass_density_handler.py
@@ -0,0 +1,39 @@
+try:
+    from PyQt4.QtGui import QDialog
+except:
+    try:
+        from PyQt5.QtWidgets import QDialog
+    except:
+        raise ImportError("Requires PyQt4 or PyQt5")
+
+from addie.ui_mass_density import Ui_Dialog as UiDialog
+
+
+class MassDensityHandler:
+
+    def __init__(self, parent=None, key=None, data_type='sample'):
+        if parent.mass_density_ui is None:
+            o_mass = MassDensityWindow(parent=parent, key=key, data_type=data_type)
+            o_mass.show()
+            parent.mass_density_ui = o_mass
+        else:
+            parent.mass_density_ui.setFocus()
+            parent.mass_density_ui.activateWindow()
+
+
+class MassDensityWindow(QDialog):
+
+    def __init__(self, parent=None, key=None, data_type='sample'):
+        self.parent = parent
+        self.key = key
+        self.data_type = data_type
+
+        QDialog.__init__(self, parent=parent)
+        self.ui = UiDialog()
+        self.ui.setupUi(self)
+
+    def accept(self):
+        pass
+
+    def reject(self):
+        pass
