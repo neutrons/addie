88732c76a3a202b9f1c22e7990e895614fee4035
Garrett Granroth <granrothge@ornl.gov>
2019-01-30 14:48:40 -0500

    added function to plot Run_diatest result 1

---
+++ tests/gr_nr_plot.py
@@ -0,0 +1,18 @@
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+# import mantid algorithms, numpy and matplotlib
+import mantid.simpleapi as ms
+from mantid import plots
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import sys
+sys.path.append('../Calibration')
+from Calibration_plots import plot_gr_nr
+    #f1.show()
+#if __name__=="__main__":
+ms.LoadNexusProcessed('diamond_gr.nxs',OutputWorkspace='gr')
+ms.LoadNexusProcessed('diamond_nr.nxs',OutputWorkspace='nr')
+f1=plot_gr_nr('gr','nr',expected_n=[4,16,28])
+f1.show()
