693bb7244fc60d5e39cd4f3a14c1b6ac9aeb6c01
JeanBilheux <bilheuxjm@ornl.gov>
2019-02-06 17:53:36 -0500

    master json is created and reaady to be imported in master table

--- addie/master_table/import_from_database/master_table_loader_from_database_ui.py
+++ addie/master_table/import_from_database/master_table_loader_from_database_ui.py
@@ -17,6 +17,10 @@ class MasterTableLoaderFromDatabaseUi:
     # json
     json = None  # list of json returned from ONCat
     reformated_json = None  # dictionary where key is run number and value is the appropriate json
+    final_json = None  # dictionary of runs, with title and list of json for those runs
+                        # {'1-3'; {'list_of_json': [json1, json2, json3],
+                        #          'title': "this is title of 1-3"},
+                        #   ..., }
 
     def __init__(self, parent=None):
         self.parent = parent
@@ -36,6 +40,10 @@ class MasterTableLoaderFromDatabaseUi:
 
         # making final json
         self._make_final_json()
+        #
+        # import pprint
+        # pprint.pprint(self.final_json)
+
 
     def _isolate_runs_and_title(self, json=None):
         """isolate the list of runs and title from the list of json returned by ONCat"""
@@ -68,24 +76,31 @@ class MasterTableLoaderFromDatabaseUi:
 
         for _entry in json:
             run_number = _entry["indexed"]["run_number"]
-            new_json[run_number] = _entry
+            new_json[str(run_number)] = _entry
 
         self.reformated_json = new_json
 
     def _make_final_json(self):
         """if runs are group together, those runs are regroup and final list of json is created"""
-        json = self.json
+        json = self.reformated_json
         list_of_runs = self.final_list_of_runs
         list_of_title = self.final_list_of_title
 
-        import pprint
-
-        final_json = []
-        for _index, _run in enumerate(list_of_runs):
+        final_json = {}
+        for _index, _combine_run in enumerate(list_of_runs):
 
             # get discrete list of the runs to isolate their json
-            o_parser = ListRunsParser(current_runs=_run)
+            o_parser = ListRunsParser(current_runs=_combine_run)
             discrete_list_of_runs = o_parser.list_current_runs
             discrete_list_of_runs.sort() # make sure the runs are in ascending order
 
-            pprint.pprint("from {} to {}".format(_run, discrete_list_of_runs))
+            list_of_json_for_this_combine_run = []
+            for _individual_run in discrete_list_of_runs:
+                list_of_json_for_this_combine_run.append(json[str(_individual_run)])
+
+            final_json[_combine_run] = {}
+            final_json[_combine_run]['list_of_json'] = list_of_json_for_this_combine_run
+            final_json[_combine_run]['title'] = list_of_title[_index]
+
+        self.final_json = final_json
+
