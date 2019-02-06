6789fc6b109f7629343bd0eab8d7b2df5bb856d9
JeanBilheux <bilheuxjm@ornl.gov>
2019-02-06 12:36:59 -0500

    Retrieving the list of files to load into the master table

--- addie/master_table/import_from_database/data_to_import_handler.py
+++ addie/master_table/import_from_database/data_to_import_handler.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class DataToImportHandler:
 
     def __init__(self, parent=None):
@@ -14,10 +17,13 @@ class DataToImportHandler:
 
         if self.is_with_filter():
             # work only with filtered runs
-            list_of_rows_to_load = self.parent.list_of_rows_with_global_rule
-            pprint.pprint(list_of_rows_to_load)
+            list_of_rows_to_load = list(self.parent.list_of_rows_with_global_rule)
 
         else:
             # work with entire stack of runs
-            nexus_json = self.parent.nexus_json_from_template
-            pprint.pprint(nexus_json)
+            nbr_rows = self.parent.ui.tableWidget_all_runs.rowCount()
+            list_of_rows_to_load = np.arange(nbr_rows)
+
+
+        pprint.pprint(list_of_rows_to_load)
+        pprint.pprint(list_of_rows_to_load[0])
