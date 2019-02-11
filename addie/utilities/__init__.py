d6e9752784b6f63facd0c00a40653c456f208f9c
McDonnell, Marshall T <mcdonnellmt@ornl.gov>
2019-02-11 09:10:20 -0500

    Fix ui designer dir location

--- addie/utilities/__init__.py
+++ addie/utilities/__init__.py
@@ -1,12 +1,15 @@
 import os
 from qtpy.uic import loadUi
+import addie
+
+addie_path =  os.path.dirname(os.path.abspath(addie.__file__))
+designer_path = os.path.join(addie_path, '../designer')
 
 def load_ui(ui_filename, baseinstance):
-    cwd = os.getcwd()
     ui_filename = os.path.split(ui_filename)[-1]
 
     # get the location of the designer directory
     # this function assumes that all ui files are there
-    filename = os.path.join(cwd, 'designer', ui_filename)
+    filename = os.path.join(designer_path, ui_filename)
 
     return loadUi(filename, baseinstance=baseinstance)
