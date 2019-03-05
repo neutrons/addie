7b13ab82366ca729b8e1be5036633dcb71800933
JeanBilheux <bilheuxjm@ornl.gov>
2019-03-05 16:35:36 -0500

    Made treelib happy with flask8

--- addie/treelib.py
+++ addie/treelib.py
@@ -261,7 +261,7 @@ class BraggTree(base.CustomizedTreeView):
                 self._mainWindow.get_workflow().delete_workspace(ws_name)
                 try:
                     self._mainWindow.remove_gss_from_plot(gss_group_name=gsas_name, gss_bank_ws_name_list=[ws_name])
-                except AssertionError as ass_err:
+                except AssertionError:
                     print('Workspace %s is not on canvas.' % ws_name)
 
             # delete the node from the tree
@@ -305,7 +305,7 @@ class BraggTree(base.CustomizedTreeView):
         selected_node = self.get_selected_items()[0]
         bank_ws_list = self.get_child_nodes(selected_node, output_str=True)
 
-        out_gss_ws = os.path.basename(new_gss_file_name).split('.')[0]
+        #out_gss_ws = os.path.basename(new_gss_file_name).split('.')[0]
         # write all the banks to a GSAS file
         self._mainWindow.get_workflow().write_gss_file(ws_name_list=bank_ws_list, gss_file_name=new_gss_file_name)
 
