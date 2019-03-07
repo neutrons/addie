a2b763906fb8007e75c54d977f3c89f24d926884
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-03-07 15:30:30 -0500

    Moved calibration resolution script

--- addie/calibration/calculation_for_dspacing_resolution_by_pixel_peak.py
+++ addie/calibration/calculation_for_dspacing_resolution_by_pixel_peak.py
@@ -5,12 +5,13 @@ from mantid.simpleapi import *
 chi2_threshold = 1e12
 
 # Input dvalues (here are the diamond ones)
-dvalues = (0.3117,0.3257,0.3499,0.4205,0.4645,0.4768,0.4996,0.5150,0.5441,0.5642,0.5947,
-           0.6307,.6866,.7283,.8185,.8920,1.0758,1.2615,2.0599)
+dvalues = (0.3117, 0.3257, 0.3499, 0.4205, 0.4645, 0.4768, 0.4996, 0.5150, 0.5441,
+           0.5642, 0.5947, 0.6307, .6866, .7283, .8185, .8920, 1.0758, 1.2615, 2.0599)
+
 
 def get_fit_params_from_instrument_preselected_file(instrument):
     # Load files
-    filename="%s_pdcalibration_diagnostics.nxs" % instrument
+    filename = "%s_pdcalibration_diagnostics.nxs" % instrument
 
     cal_wksp = LoadNexusProcessed(Filename=filename)
 
@@ -21,8 +22,10 @@ def get_fit_params_from_instrument_preselected_file(instrument):
         wksp_name = "PG3_39169_diag_fitparam"
     return mtd[wksp_name]
 
+
 def get_fit_params_by_fitting(wksp):
     pass
+
 
 def get_column_data(wksp, val_col, chi2_col, threshold):
     values = list()
@@ -31,12 +34,14 @@ def get_column_data(wksp, val_col, chi2_col, threshold):
             values.append(val)
     return values
 
+
 def deltaD(tof_center, tof_width, dspace_center):
-    tof_fwhm = 2.* np.sqrt(2.*np.log(2.)) * tof_width
+    tof_fwhm = 2. * np.sqrt(2. * np.log(2.)) * tof_width
     dspace_fwhm = (tof_fwhm / tof_center) * dspace_center
     return dspace_fwhm
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     wksp = get_fit_params_from_instrument_preselected_file("NOM")
 
     # Extract the column IDs of interest
@@ -52,10 +57,11 @@ if __name__=="__main__":
     width_vals = get_column_data(wksp, width_col, chi2_col, chi2_threshold)
     dspace_idx_vals = get_column_data(wksp, peak_col, chi2_col, chi2_threshold)
 
-    # Make array where rows are pixels and cols are delta-d values (FWHM in dspace)
+    # Make array where rows are pixels and cols are delta-d values (FWHM in
+    # dspace)
     pixels = np.unique(np.array(pixel_idx_vals))
-    idx2pixel = { i:pid for i, pid in enumerate(pixels) }
-    pixel2idx = { pid:i for i, pid in enumerate(pixels)}
+    idx2pixel = {i: pid for i, pid in enumerate(pixels)}
+    pixel2idx = {pid: i for i, pid in enumerate(pixels)}
 
     '''
      TODO: vectorize the for-loop to operate on arrays for : tof_center, tof_widths, dvalues
@@ -69,14 +75,13 @@ if __name__=="__main__":
     '''
     pixel_deltaD = np.zeros((len(pixels), len(dvalues)))
     fig, ax = plt.subplots(1)
-    for pidx, tof_center, tof_width, dspace_idx in zip(pixel_idx_vals, center_vals, width_vals, dspace_idx_vals):
+    for pidx, tof_center, tof_width, dspace_idx in zip(
+            pixel_idx_vals, center_vals, width_vals, dspace_idx_vals):
         dspace_width = deltaD(tof_center, tof_width, dvalues[dspace_idx])
-        pixel_deltaD[pixel2idx[pidx],dspace_idx] = dspace_width
+        pixel_deltaD[pixel2idx[pidx], dspace_idx] = dspace_width
 
     for idx, row in enumerate(pixel_deltaD):
         ax.plot(dvalues, row, 'o', label="Pixel: %d" % idx2pixel[idx])
 
     ax.legend()
     plt.show()
-        
-         
