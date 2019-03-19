dc109fd6bfce7cdf45849832c2193c7055971f64
Pete Peterson <petersonpf@ornl.gov>
2019-03-19 09:30:24 -0400

    Split classes out into separate files

---
+++ addie/plot/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .canvas import Qt4MplCanvas  # noqa
+from .navigationtoolbar import MyNavigationToolbar  # noqa
+from .indicatormanager import IndicatorManager  # noqa
