933be6eb40362583ddd9b63ee005905a7b6cbeed
Pete Peterson <petersonpf@ornl.gov>
2019-03-21 12:17:04 -0400

    Remove unused import

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, print_function
 import os
-import unittest
 
 DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 print('looking for data in "{}"'.format(DATA_DIR))
