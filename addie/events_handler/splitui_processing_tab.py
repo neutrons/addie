19bc8f0ef0db9733c2aa134b562f89206a05f2e9
JeanBilheux <bilheuxjm@ornl.gov>
2019-03-27 11:28:13 -0400

    Added connection to bottom button of mantid (processing) tab)

--- addie/events_handler/splitui_processing_tab.py
+++ addie/events_handler/splitui_processing_tab.py
@@ -4,6 +4,8 @@ from qtpy import QtGui
 def run(main_window=None):
     main_window.processing_ui.clear_search_button.clicked.connect(main_window.table_search_clear)
     main_window.processing_ui.settings_table_button.clicked.connect(main_window.personalization_table_clicked)
-
+    main_window.processing_ui.reduction_configuration_button.clicked.connect(main_window.reduction_configuration_button_clicked)
+    main_window.processing_ui.browse_calibration_button.clicked.connect(main_window.browse_calibration_clicked)
+    main_window.processing_ui.make_calibration_button.clicked.connect(main_window.make_calibration_clicked)
 
 
