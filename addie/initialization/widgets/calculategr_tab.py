b8dabb2469b171f413782e7e891c0ffbed7f0152
JeanBilheux <bilheuxjm@ornl.gov>
2019-04-02 09:51:47 -0400

    main window starts correctly now with caluclate_gr ui splitted

--- addie/initialization/widgets/calculategr_tab.py
+++ addie/initialization/widgets/calculategr_tab.py
@@ -27,3 +27,47 @@ def run(main_window=None):
     temp_layout.addWidget(main_window.calculategr_ui.treeWidget_grWsList)
 
 
+    main_window.calculategr_ui.splitter_4.setStyleSheet("""
+                                                        QSplitter::handle {
+                                                           image: url(':/MPL Toolbar/splitter_icon.png');
+                                                        }
+                                                        """)
+
+    main_window.calculategr_ui.splitter_5.setStyleSheet("""
+                                                        QSplitter::handle {
+                                                           image: url(':/MPL Toolbar/vertical_splitter_icon.png');
+                                                        }
+                                                        """)
+
+    main_window.calculategr_ui.treeWidget_grWsList.set_main_window(main_window)
+    main_window.calculategr_ui.treeWidget_grWsList.add_main_item('workspaces',
+                                                                 append=True,
+                                                                 as_current_index=False)
+    main_window.calculategr_ui.treeWidget_grWsList.add_main_item('SofQ',
+                                                                 append=True,
+                                                                 as_current_index=False)
+
+    main_window.calculategr_ui.comboBox_SofQType.clear()
+    main_window.calculategr_ui.comboBox_SofQType.addItem('S(Q)')
+    main_window.calculategr_ui.comboBox_SofQType.addItem('S(Q)-1')
+    main_window.calculategr_ui.comboBox_SofQType.addItem('Q[S(Q)-1]')
+    main_window.calculategr_ui.comboBox_SofQType.setCurrentIndex(0)
+
+
+    main_window.calculategr_ui.comboBox_pdfType.addItems(['G(r)', 'g(r)', 'RDF(r)'])
+
+    # some starting value
+    main_window.calculategr_ui.doubleSpinBoxDelR.setValue(0.01)
+
+    # set a constant item to combobox Sq
+    main_window.calculategr_ui.comboBox_SofQ.addItem('All')
+
+    # PDF filter
+    main_window.calculategr_ui.comboBox_pdfCorrection.clear()
+    main_window.calculategr_ui.comboBox_pdfCorrection.addItem('No Modification')
+    main_window.calculategr_ui.comboBox_pdfCorrection.addItem('Lorch')
+
+    # set the lower limit on Qmin
+    main_window.calculategr_ui.doubleSpinBoxQmin.setDecimals(6)
+    main_window.calculategr_ui.doubleSpinBoxQmin.setMinimum(1.E-10)
+
