cdd0e5fd4fe0a1979e9eeebe9a427535733dba03
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-03 11:13:22 -0400

    Updated mantid_total_scattering submodule

--- update_submodule.sh
+++ update_submodule.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-git submodule update --init --recursive
+# For the first time, use this
+#git submodule update --init --recursive
+
+# To pull the last commit on all submodules
+git submodule foreach git pull origin master
