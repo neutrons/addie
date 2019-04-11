22ef34934088efcee76275e0864ec006658acf8f
JeanBilheux <bilheuxjm@ornl.gov>
2019-04-11 13:55:28 -0400

    list of keys is now predetermined and unique. This refs #222

--- addie/processing/mantid/master_table/align_and_focus_args.py
+++ addie/processing/mantid/master_table/align_and_focus_args.py
@@ -28,6 +28,8 @@ class AlignAndFocusArgsWindow(QMainWindow):
     list_algo_without_blacklist = None
     unused_list_algo = None
 
+    local_list_key_loaded = []
+
     def __init__(self, main_window=None, key=None):
         self.main_window = main_window
         self.key = key
@@ -73,16 +75,26 @@ class AlignAndFocusArgsWindow(QMainWindow):
 
         previous_key_value_dict = self._get_previous_key_value_dict()
         list_key_already_loaded = previous_key_value_dict.keys()
+        global_unused_list_algo = self.remove_from_list(original_list=list_algo,
+                                                        to_remove=list_key_already_loaded)
 
-        if list_key_already_loaded:
+        local_list_key_loaded = self.local_list_key_loaded
+        global_and_local_unused_list_algo = self.remove_from_list(original_list=global_unused_list_algo,
+                                                                  to_remove=local_list_key_loaded)
+
+        self.unused_list_algo = global_and_local_unused_list_algo
+
+    def remove_from_list(self,
+                         original_list=[],
+                         to_remove=[]):
+        if to_remove:
             clean_list_algo = []
-            for _algo in list_algo:
-                if not(_algo in list_key_already_loaded):
+            for _algo in original_list:
+                if not(_algo in to_remove):
                     clean_list_algo.append(_algo)
-            self.unused_list_algo = clean_list_algo
-
+            return clean_list_algo
         else:
-            self.unused_list_algo = list_algo
+            return original_list
 
     def _set_column_widths(self):
         for _col, _width in enumerate(COLUMNS_WIDTH):
@@ -93,6 +105,7 @@ class AlignAndFocusArgsWindow(QMainWindow):
         for _row, _key in enumerate(previous_key_value_dict.keys()):
             _value = previous_key_value_dict[_key]
             self._add_row(row=_row, key=_key, value=_value)
+            self.local_list_key_loaded.append(_key)
 
     def _get_previous_key_value_dict(self):
         master_table_list_ui = self.main_window.master_table_list_ui[self.key]
@@ -125,9 +138,11 @@ class AlignAndFocusArgsWindow(QMainWindow):
     def _add_new_row_at_bottom(self):
         nbr_row = self.get_nbr_row()
         key = self.get_current_selected_key()
+        self.local_list_key_loaded.append(key)
         value = str(self.ui.new_value_widget.text())
         self._add_row(row=nbr_row, key=key, value=value)
         self.ui.new_value_widget.setText("")
+        self.populate_list_algo()
 
     def get_current_selected_key(self):
         return str(self.ui.list_key_comboBox.currentText())
