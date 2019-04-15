845c10c07a35c32d54680889a4ed0e43df470df4
JeanBilheux <bilheuxjm@ornl.gov>
2019-04-15 17:07:41 -0400

    Prototypes background cells color for items and widgets (just one of them so far)

--- addie/processing/mantid/master_table/column_highlighting.py
+++ addie/processing/mantid/master_table/column_highlighting.py
@@ -1,4 +1,15 @@
-from addie.processing.mantid.master_table.tree_definition import LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING
+import numpy as np
+from qtpy import QtGui
+from qtpy.QtWidgets import QTableWidgetItem
+
+from addie.processing.mantid.master_table.tree_definition import (INDEX_OF_COLUMNS_SEARCHABLE,
+                                                                  INDEX_OF_COLUMNS_WITH_CHEMICAL_FORMULA,
+                                                                  INDEX_OF_COLUMNS_WITH_MASS_DENSITY,
+                                                                  INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS,
+                                                                  INDEX_OF_ABS_CORRECTION,
+                                                                  INDEX_OF_MULTI_SCATTERING_CORRECTION,
+                                                                  INDEX_OF_INELASTIC_CORRECTION,
+                                                                  LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING)
 
 
 class ColumnHighlighting:
@@ -7,6 +18,78 @@ class ColumnHighlighting:
         self.main_window = main_window
         self.column = column
 
+        self.nbr_row = self.get_nbr_row()
+
+    def get_nbr_row(self):
+        nbr_row = self.main_window.processing_ui.h3_table.rowCount()
+        return nbr_row
+
     def check(self):
-        print("checking column {}".format(self.column))
+        column = self.column
+
+        for _row in np.arange(self.nbr_row):
+
+            # item
+            if column in INDEX_OF_COLUMNS_SEARCHABLE:
+                are_all_the_same = self.are_cells_identical()
+
+            elif column in INDEX_OF_COLUMNS_WITH_CHEMICAL_FORMULA:
+                are_all_the_same = self.are_chemical_formula_identical()
+
+            elif column in INDEX_OF_COLUMNS_WITH_MASS_DENSITY:
+                are_all_the_same = self.are_mass_density_identical()
+
+            elif column in INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS:
+                are_all_the_same = self.are_geometry_identical()
+
+            elif (column in INDEX_OF_ABS_CORRECTION) or \
+                    (column in INDEX_OF_MULTI_SCATTERING_CORRECTION):
+                are_all_the_same = self.are_abs_or_multi_correction_identical()
+
+            elif column in INDEX_OF_INELASTIC_CORRECTION:
+                are_all_the_same = self.are_inelastic_correction_identical()
+
+            elif column in LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING[-1]:
+                are_all_the_same = self.are_align_and_focus_args_identical()
+
+        self.apply_cell_background(are_all_the_same=are_all_the_same)
+
+    def apply_cell_background(self, are_all_the_same=False):
+        if are_all_the_same:
+            background_color = QtGui.QColor(255, 255, 0)
+            background_color_stylesheet = "rgb(255, 255, 0)"
+        else:
+            background_color = QtGui.QColor(255, 255, 255)
+            background_color_stylesheet = "rgb(255, 255, 255)"
+
+        for _row in np.arange(self.nbr_row):
+
+            if self.column == 5:
+                main_widget = self.main_window.processing_ui.h3_table.cellWidget(_row, self.column)
+                main_widget.setAutoFillBackground(True)
+                main_widget.setStyleSheet("background-color: {};".format(background_color_stylesheet))
+            else:
+                self.main_window.processing_ui.h3_table.item(_row, self.column).setBackground(background_color)
+
+
+    def are_cells_identical(self):
+        return True
+
+    def are_chemical_formula_identical(self):
+        return True
+
+    def are_mass_density_identical(self):
+        return False
+
+    def are_geometry_identical(self):
+        return False
+
+    def are_abs_or_multi_correction_identical(self):
+        return False
+
+    def are_inelastic_correction_identical(self):
+        return False
+
+    def are_align_and_focus_args_identical(self):
+        return False
 
