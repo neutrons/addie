f1b1faf99019a44761fbbe7a656369294f7e89e1
JeanBilheux <bilheuxjm@ornl.gov>
2019-04-15 17:31:39 -0400

    Framework of checking for column identical values is in place

--- addie/processing/mantid/master_table/column_highlighting.py
+++ addie/processing/mantid/master_table/column_highlighting.py
@@ -5,6 +5,7 @@ from qtpy.QtWidgets import QTableWidgetItem
 from addie.processing.mantid.master_table.tree_definition import (INDEX_OF_COLUMNS_SEARCHABLE,
                                                                   INDEX_OF_COLUMNS_WITH_CHEMICAL_FORMULA,
                                                                   INDEX_OF_COLUMNS_WITH_MASS_DENSITY,
+                                                                  INDEX_OF_COLUMNS_SHAPE,
                                                                   INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS,
                                                                   INDEX_OF_ABS_CORRECTION,
                                                                   INDEX_OF_MULTI_SCATTERING_CORRECTION,
@@ -26,8 +27,9 @@ class ColumnHighlighting:
 
     def check(self):
         column = self.column
+        are_all_the_same = False
 
-        for _row in np.arange(self.nbr_row):
+        for _ in np.arange(self.nbr_row):
 
             # item
             if column in INDEX_OF_COLUMNS_SEARCHABLE:
@@ -39,6 +41,9 @@ class ColumnHighlighting:
             elif column in INDEX_OF_COLUMNS_WITH_MASS_DENSITY:
                 are_all_the_same = self.are_mass_density_identical()
 
+            elif column in INDEX_OF_COLUMNS_SHAPE:
+                are_all_the_same = self.are_shape_identical()
+
             elif column in INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS:
                 are_all_the_same = self.are_geometry_identical()
 
@@ -49,7 +54,7 @@ class ColumnHighlighting:
             elif column in INDEX_OF_INELASTIC_CORRECTION:
                 are_all_the_same = self.are_inelastic_correction_identical()
 
-            elif column in LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING[-1]:
+            elif column == LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING[-1]:
                 are_all_the_same = self.are_align_and_focus_args_identical()
 
         self.apply_cell_background(are_all_the_same=are_all_the_same)
@@ -64,13 +69,19 @@ class ColumnHighlighting:
 
         for _row in np.arange(self.nbr_row):
 
-            if self.column == 5:
+            if (self.column in INDEX_OF_COLUMNS_WITH_CHEMICAL_FORMULA) or \
+                    (self.column in INDEX_OF_COLUMNS_WITH_MASS_DENSITY) or \
+                    (self.column in INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS) or \
+                    (self.column in INDEX_OF_COLUMNS_SHAPE) or \
+                    (self.column in INDEX_OF_ABS_CORRECTION) or \
+                    (self.column in INDEX_OF_MULTI_SCATTERING_CORRECTION) or \
+                    (self.column in INDEX_OF_INELASTIC_CORRECTION) or \
+                    (self.column == LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING):
                 main_widget = self.main_window.processing_ui.h3_table.cellWidget(_row, self.column)
                 main_widget.setAutoFillBackground(True)
                 main_widget.setStyleSheet("background-color: {};".format(background_color_stylesheet))
             else:
                 self.main_window.processing_ui.h3_table.item(_row, self.column).setBackground(background_color)
-
 
     def are_cells_identical(self):
         return True
@@ -79,17 +90,20 @@ class ColumnHighlighting:
         return True
 
     def are_mass_density_identical(self):
-        return False
+        return True
+
+    def are_shape_identical(self):
+        return True
 
     def are_geometry_identical(self):
-        return False
+        return True
 
     def are_abs_or_multi_correction_identical(self):
-        return False
+        return True
 
     def are_inelastic_correction_identical(self):
-        return False
+        return True
 
     def are_align_and_focus_args_identical(self):
-        return False
+        return True
 
