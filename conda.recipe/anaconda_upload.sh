0126cab21ac8e80ae23ac8859ddc80263aaa2e90
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-04-30 11:46:15 -0400

    Added mantidTS channel, libGL install, and updates to upload

--- conda.recipe/anaconda_upload.sh
+++ conda.recipe/anaconda_upload.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# Inputs
-OS=${TRAVIS_OS_NAME}-64
+# Input
+if [[ $# -ne 1 ]]
+then
+  echo "Usage: anaconda_upload.sh <python version>"
+  exit 1
+fi
+export PYTHON_VERSION=$1
 
 # Setup anaconda for upload.
 #  NOTE: client set since there was a regression and this one shows errors.
@@ -11,25 +16,22 @@ conda install -q conda-build anaconda-client=1.5.5
 # Function for building recipe and then uploading package:
 # Usage example: conda_build_and_upload build_dir meta.yaml main
 function conda_build_and_upload {
-    if [[ $# -ne 1 ]]
+    if [[ $# -ne 2 ]]
     then
-      echo "Usage: conda_build <package directory>"
+      echo "Usage: conda_build <os type> <package directory>"
       exit 1
     fi
+    export OS=$1
+    export PKG_NAME=$2
 
-    export PKG_NAME=$1
+	conda build . --python=${PYTHON_VERSION}
+    PKG_PATH=$(conda build ${PKG_NAME} --python=${PYTHON_VERSION} --output)
+    PKG_FILE=$(basename ${pkg_path})
 
-	export CONDA_BLD_PATH=$HOME/conda-bld
-    rm -rf ${CONDA_BLD_PATH}
-	mkdir ${CONDA_BLD_PATH}
+    echo "Uploading ${PKG_FILE} artifact..."
+    anaconda -v -t ${CONDA_UPLOAD_TOKEN} upload ${PKG_PATH} --force
 
-	conda build .
-	
-	export BUILD=$(ls ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}* | sed -n "s/.*${PKG_NAME}-\(.*\)-\(.*\)\.tar.bz2/\1-\2/p")
-	echo "Uploading ${PKG_NAME}-${BUILD}.tar.bz2 artifact..."
-	anaconda -v -t ${CONDA_UPLOAD_TOKEN} upload ${CONDA_BLD_PATH}/${OS}/${PKG_NAME}-${BUILD}.tar.bz2 --force
-
-	echo "Successfully deployed ${PKG_NAME}-${BUILD} to Anaconda.org."
+    echo "Successfully deployed ${PKG_FILE} to Anaconda.org."
 }
 
 conda_build_and_upload addie
