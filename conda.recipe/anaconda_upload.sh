c3e9ec40f1cddf7d0b0f64a19e77bd41ba6e5439
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-05-13 09:10:01 -0400

    Refactor of conda setup + deploy in travis CI

--- conda.recipe/anaconda_upload.sh
+++ conda.recipe/anaconda_upload.sh
@@ -1,36 +1,16 @@
 #!/usr/bin/env bash
-
+  
 # Input
 if [[ $# -ne 1 ]]
 then
-  echo "Usage: anaconda_upload.sh <python version>"
+  echo "Usage: anaconda_upload.sh <full package path>"
   exit 1
 fi
-export PYTHON_VERSION=$1
 
-# Setup anaconda for upload.
-#  NOTE: client set since there was a regression and this one shows errors.
-conda config --set anaconda_upload no
-conda install -q conda-build anaconda-client=1.5.5
+PKG_PATH=$1
+PKG_FILE=$(basename ${PKG_PATH})
 
-# Function for building recipe and then uploading package:
-# Usage example: conda_build_and_upload build_dir meta.yaml main
-function conda_build_and_upload {
-    if [[ $# -ne 1 ]]
-    then
-      echo "Usage: conda_build <os type>"
-      exit 1
-    fi
-    export OS=$1
+echo "Uploading ${PKG_PATH} artifact..."
+anaconda -v -t ${CONDA_UPLOAD_TOKEN} upload ${PKG_PATH} --force
 
-	conda build conda.recipe --python=${PYTHON_VERSION}
-    PKG_PATH=$(conda build conda.recipe --python=${PYTHON_VERSION} --output)
-    PKG_FILE=$(basename ${PKG_PATH})
-
-    echo "Uploading ${PKG_FILE} artifact..."
-    anaconda -v -t ${CONDA_UPLOAD_TOKEN} upload ${PKG_PATH} --force
-    echo "error code: $#"
-    echo "Successfully deployed ${PKG_FILE} to Anaconda.org."
-}
-
-conda_build_and_upload ${TRAVIS_OS_NAME}-64 
+echo "Successfully deployed ${PKG_FILE} to Anaconda.org."
