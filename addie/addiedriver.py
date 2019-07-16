811e39fd8df30cf86ebb56b7d04ea66b16219c32
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-07-16 10:41:57 -0400

    Update mantid.plots.helperfuncions.get_spectrum call
    This was changed in this PR in Mantid:
    https://github.com/mantidproject/mantid/pull/25756

--- addie/addiedriver.py
+++ addie/addiedriver.py
@@ -341,7 +341,7 @@ class AddieDriver(object):
     @staticmethod
     def get_ws_data(ws_name, wkspIndex=0, withDy=True):
         wksp = AddieDriver.get_ws(ws_name)
-        x, y, dy, _ = mantid.plots.helperfunctions.get_spectrum(wksp, wkspIndex, distribution=True, withDy=withDy, withDx=False)
+        x, y, dy, _ = mantid.plots.helperfunctions.get_spectrum(wksp, wkspIndex, False, withDy=withDy, withDx=False)
 
         return x, y, dy
 
