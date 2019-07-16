36e8fe3572137ee6b0951d02b1b245dc52fea7eb
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-07-16 14:33:23 -0400

    Formatting for column_highlighting

--- addie/processing/mantid/master_table/column_highlighting.py
+++ addie/processing/mantid/master_table/column_highlighting.py
@@ -3,18 +3,24 @@ from qtpy import QtGui
 
 from addie.processing.mantid.master_table.utilities import Utilities
 
-from addie.processing.mantid.master_table.tree_definition import (INDEX_OF_COLUMNS_SEARCHABLE,
-                                                                  INDEX_OF_COLUMNS_WITH_CHEMICAL_FORMULA,
-                                                                  INDEX_OF_COLUMNS_WITH_MASS_DENSITY,
-                                                                  INDEX_OF_COLUMNS_SHAPE,
-                                                                  INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS,
-                                                                  INDEX_OF_ABS_CORRECTION,
-                                                                  INDEX_OF_MULTI_SCATTERING_CORRECTION,
-                                                                  INDEX_OF_INELASTIC_CORRECTION,
-                                                                  LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING)
-from addie.processing.mantid.master_table.tree_definition import (COLUMNS_IDENTICAL_VALUES_COLOR,
-                                                                  COLUMNS_DIFFERENT_VALUES_COLOR)
-from addie.processing.mantid.master_table.tree_definition import INDEX_NORMALIZATION_START
+from addie.processing.mantid.master_table.tree_definition import (
+    INDEX_OF_COLUMNS_SEARCHABLE,
+    INDEX_OF_COLUMNS_WITH_CHEMICAL_FORMULA,
+    INDEX_OF_COLUMNS_WITH_MASS_DENSITY,
+    INDEX_OF_COLUMNS_SHAPE,
+    INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS,
+    INDEX_OF_ABS_CORRECTION,
+    INDEX_OF_MULTI_SCATTERING_CORRECTION,
+    INDEX_OF_INELASTIC_CORRECTION,
+    LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING
+)
+from addie.processing.mantid.master_table.tree_definition import (
+    COLUMNS_IDENTICAL_VALUES_COLOR,
+    COLUMNS_DIFFERENT_VALUES_COLOR
+)
+from addie.processing.mantid.master_table.tree_definition import (
+    INDEX_NORMALIZATION_START
+)
 
 
 class ColumnHighlighting:
@@ -47,7 +53,7 @@ class ColumnHighlighting:
         self.main_window.processing_ui.h3_table.blockSignals(True)
 
         self.column = column
-        are_all_the_same = False
+        are_all_same = False
 
         self.set_data_type()
 
@@ -57,50 +63,58 @@ class ColumnHighlighting:
 
                 # item
                 if column in INDEX_OF_COLUMNS_SEARCHABLE:
-                    are_all_the_same = self.are_cells_identical()
+                    are_all_same = self.are_cells_identical()
 
                 elif column in INDEX_OF_COLUMNS_WITH_CHEMICAL_FORMULA:
-                    are_all_the_same = self.are_chemical_formula_identical()
+                    are_all_same = self.are_chemical_formula_identical()
 
                 elif column in INDEX_OF_COLUMNS_WITH_MASS_DENSITY:
-                    are_all_the_same = self.are_mass_density_identical()
+                    are_all_same = self.are_mass_density_identical()
 
                 elif column in INDEX_OF_COLUMNS_SHAPE:
-                    are_all_the_same = self.are_shape_identical()
+                    are_all_same = self.are_shape_identical()
 
                 elif column in INDEX_OF_COLUMNS_WITH_GEOMETRY_INFOS:
-                    are_all_the_same = self.are_geometry_identical()
+                    are_all_same = self.are_geometry_identical()
 
                 elif column in INDEX_OF_ABS_CORRECTION:
-                    are_all_the_same = self.are_abs_correction_identical()
+                    are_all_same = self.are_abs_correction_identical()
 
                 elif column in INDEX_OF_MULTI_SCATTERING_CORRECTION:
-                    are_all_the_same = self.are_multi_correction_identical()
+                    are_all_same = self.are_multi_correction_identical()
 
                 elif column in INDEX_OF_INELASTIC_CORRECTION:
-                    are_all_the_same = self.are_inelastic_correction_identical()
+                    are_all_same = self.are_inelastic_correction_identical()
 
                 elif column == LIST_COLUMNS_TO_SEARCH_FOR_FULL_HIGHLIGTHING[-1]:
-                    are_all_the_same = self.are_align_and_focus_args_identical()
+                    are_all_same = self.are_align_and_focus_args_identical()
 
-        self.apply_cell_background(are_all_the_same=are_all_the_same)
+        self.apply_cell_background(are_all_the_same=are_all_same)
         self.main_window.processing_ui.h3_table.blockSignals(False)
 
     def apply_cell_background(self, are_all_the_same=False):
         if are_all_the_same:
-            background_color = QtGui.QColor(COLUMNS_IDENTICAL_VALUES_COLOR[0],
-                                            COLUMNS_IDENTICAL_VALUES_COLOR[1],
-                                            COLUMNS_IDENTICAL_VALUES_COLOR[2])
-            background_color_stylesheet = "rgb({}, {}, {})".format(COLUMNS_IDENTICAL_VALUES_COLOR[0],
-                                                                   COLUMNS_IDENTICAL_VALUES_COLOR[1],
-                                                                   COLUMNS_IDENTICAL_VALUES_COLOR[2])
+            background_color = QtGui.QColor(
+                COLUMNS_IDENTICAL_VALUES_COLOR[0],
+                COLUMNS_IDENTICAL_VALUES_COLOR[1],
+                COLUMNS_IDENTICAL_VALUES_COLOR[2]
+            )
+            background_color_stylesheet = "rgb({}, {}, {})".format(
+                COLUMNS_IDENTICAL_VALUES_COLOR[0],
+                COLUMNS_IDENTICAL_VALUES_COLOR[1],
+                COLUMNS_IDENTICAL_VALUES_COLOR[2]
+            )
         else:
-            background_color = QtGui.QColor(COLUMNS_DIFFERENT_VALUES_COLOR[0],
-                                            COLUMNS_DIFFERENT_VALUES_COLOR[1],
-                                            COLUMNS_DIFFERENT_VALUES_COLOR[2])
-            background_color_stylesheet = "rgb({}, {}, {})".format(COLUMNS_DIFFERENT_VALUES_COLOR[0],
-                                                                   COLUMNS_DIFFERENT_VALUES_COLOR[1],
-                                                                   COLUMNS_DIFFERENT_VALUES_COLOR[2])
+            background_color = QtGui.QColor(
+                COLUMNS_DIFFERENT_VALUES_COLOR[0],
+                COLUMNS_DIFFERENT_VALUES_COLOR[1],
+                COLUMNS_DIFFERENT_VALUES_COLOR[2]
+            )
+            background_color_stylesheet = "rgb({}, {}, {})".format(
+                COLUMNS_DIFFERENT_VALUES_COLOR[0],
+                COLUMNS_DIFFERENT_VALUES_COLOR[1],
+                COLUMNS_DIFFERENT_VALUES_COLOR[2]
+            )
 
         pen_color = "black"
         selection_color = "white"
@@ -108,28 +122,36 @@ class ColumnHighlighting:
 
         for _row in np.arange(self.nbr_row):
 
-            main_widget = self.main_window.processing_ui.h3_table.cellWidget(_row, self.column)
+            main_widget = self.main_window.processing_ui.h3_table.cellWidget(
+                _row, self.column)
 
             if main_widget:
 
                 if self.column in INDEX_OF_COLUMNS_SEARCHABLE:
-                    self.main_window.processing_ui.h3_table.item(_row, self.column).setBackground(background_color)
+                    self.main_window.processing_ui.h3_table.item(
+                        _row, self.column).setBackground(background_color)
                 else:
                     main_widget.setAutoFillBackground(True)
-                    main_widget.setStyleSheet("color: {}; "
-                                              "background-color: {};"
-                                              "selection-color: {};"
-                                              "selection-background-color: {};".format(pen_color,
-                                                                                       background_color_stylesheet,
-                                                                                       selection_color,
-                                                                                       selection_background_color))
+                    main_widget.setStyleSheet(
+                        "color: {}; "
+                        "background-color: {};"
+                        "selection-color: {};"
+                        "selection-background-color: {};".format(
+                            pen_color,
+                            background_color_stylesheet,
+                            selection_color,
+                            selection_background_color
+                        )
+                    )
 
             else:
-                self.main_window.processing_ui.h3_table.item(_row, self.column).setBackground(background_color)
+                self.main_window.processing_ui.h3_table.item(
+                    _row, self.column).setBackground(background_color)
 
     def are_cells_identical(self):
         def _get_item_value(row=-1, column=-1):
-            return str(self.main_window.processing_ui.h3_table.item(row, column).text())
+            item = self.main_window.processing_ui.h3_table.item(row, column)
+            return str(item.text())
 
         ref_value = _get_item_value(0, self.column)
         for _row in np.arange(1, self.nbr_row):
@@ -230,33 +252,42 @@ class ColumnHighlighting:
     def _get_master_table_list_ui_for_row(self, row=-1):
         o_utilities = Utilities(parent=self.main_window)
         key_from_row = o_utilities.get_row_key_from_row_index(row=row)
-        master_table_list_ui = self.main_window.master_table_list_ui[key_from_row]
-        return master_table_list_ui
+        table_list_ui = self.main_window.master_table_list_ui[key_from_row]
+        return table_list_ui
 
     def _get_chemical_formula_widget_value(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
-        widget_ui = master_table_list_ui_for_row[self.data_type]['material']['text']
-        return str(widget_ui.text())
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
+        widget_ui = master_table_list_ui_for_row[self.data_type]
+        return str(widget_ui['material']['text'].text())
 
     def _get_mass_density_widget_value(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
-        widget_ui = master_table_list_ui_for_row[self.data_type]['mass_density']['text']
-        return str(widget_ui.text())
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
+        widget_ui = master_table_list_ui_for_row[self.data_type]
+        return str(widget_ui['mass_density']['text'].text())
 
     def _get_shape_value(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
         widget_ui = master_table_list_ui_for_row[self.data_type]['shape']
         return str(widget_ui.currentText())
 
     # geometry Dimensions column
     def get_geometry_dimensions_widgets(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
-        radius_ui = master_table_list_ui_for_row[self.data_type]['geometry']['radius']['value']
-        radius2_ui = master_table_list_ui_for_row[self.data_type]['geometry']['radius2']['value']
-        height_ui = master_table_list_ui_for_row[self.data_type]['geometry']['height']['value']
-        return {'radius': radius_ui,
-                'radius2': radius2_ui,
-                'height': height_ui}
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
+        geometry = master_table_list_ui_for_row[self.data_type]['geometry']
+
+        radius_ui = geometry['radius']['value']
+        radius2_ui = geometry['radius2']['value']
+        height_ui = geometry['height']['value']
+
+        return {
+            'radius': radius_ui,
+            'radius2': radius2_ui,
+            'height': height_ui
+        }
 
     def _are_radius_identical(self):
         return self._are_geometry_value_identical(variable_name='radius')
@@ -278,26 +309,31 @@ class ColumnHighlighting:
         return True
 
     def _get_abs_correction_widget_value(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
-        widget_ui = master_table_list_ui_for_row[self.data_type]['abs_correction']
-        return str(widget_ui.currentText())
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
+        widget_ui = master_table_list_ui_for_row[self.data_type]
+        return str(widget_ui['abs_correction'].currentText())
 
     def _get_multi_correction_widget_value(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
-        widget_ui = master_table_list_ui_for_row[self.data_type]['mult_scat_correction']
-        return str(widget_ui.currentText())
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
+        widget_ui = master_table_list_ui_for_row[self.data_type]
+        return str(widget_ui['mult_scat_correction'].currentText())
 
     def _get_inelastic_correction_widget_value(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
-        widget_ui = master_table_list_ui_for_row[self.data_type]['inelastic_correction']
-        return str(widget_ui.currentText())
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
+        widget_ui = master_table_list_ui_for_row[self.data_type]
+        return str(widget_ui['inelastic_correction'].currentText())
 
     def _get_placzek_infos(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
         dict = master_table_list_ui_for_row[self.data_type]['placzek_infos']
         return dict
 
     def _get_align_and_focus_args_value(self, row=-1):
-        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(row=row)
+        master_table_list_ui_for_row = self._get_master_table_list_ui_for_row(
+            row=row)
         dict = master_table_list_ui_for_row['align_and_focus_args_infos']
         return dict
