06ce225ee1639fe945c75cb7ee7e07d2986bf79c
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-08-01 07:57:37 -0400

    Flake8 warnings

--- tests/rietveld/test_braggtree.py
+++ tests/rietveld/test_braggtree.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function
 import pytest
-from addie.main import MainWindow
 from addie.rietveld.braggtree import BraggTree, BankRegexException
+
 
 @pytest.fixture
 def bragg_tree():
@@ -15,13 +15,15 @@ def test_get_bank_id(qtbot, bragg_tree):
     bank_id = bragg_tree._get_bank_id(bank_wksp_name)
     assert int(bank_id) == target
 
+
 def test_get_bank_id_exception(qtbot, bragg_tree):
     """Test for raised exception from a bad workspace name"""
     bad_ws = "Bank jkl 1 -- 90.0"
-    with pytest.raises(BankRegexException) as e:
+    with pytest.raises(BankRegexException):
         bragg_tree._get_bank_id(bad_ws)
+
 
 def test_do_plot_ws_exception(qtbot, bragg_tree):
     """Test for raised exception from MainWindow==None"""
-    with pytest.raises(NotImplementedError) as e:
+    with pytest.raises(NotImplementedError):
         bragg_tree.do_plot_ws()
--- tests/rietveld/test_event_handler.py
+++ tests/rietveld/test_event_handler.py
@@ -10,5 +10,5 @@ def rietveld_event_handler(qtbot):
 
 def test_evt_change_gss_mode_exception(qtbot, rietveld_event_handler):
     """Test we can extract a bank id from bank workspace name"""
-    with pytest.raises(NotImplementedError) as e:
-         rietveld_event_handler.evt_change_gss_mode(None)
+    with pytest.raises(NotImplementedError):
+        rietveld_event_handler.evt_change_gss_mode(None)
