5fb34c1fc8d6aba7cb0ef71ffdf44f8dee9442e6
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-08-01 07:53:07 -0400

    Fixed naming in BraggTree testing

--- tests/rietveld/test_braggtree.py
+++ tests/rietveld/test_braggtree.py
@@ -4,24 +4,24 @@ from addie.main import MainWindow
 from addie.rietveld.braggtree import BraggTree, BankRegexException
 
 @pytest.fixture
-def braggtree():
+def bragg_tree():
     return BraggTree(None)
 
 
-def test_get_bank_id(qtbot, braggtree):
+def test_get_bank_id(qtbot, bragg_tree):
     """Test we can extract a bank id from bank workspace name"""
     target = 12345
     bank_wksp_name = "Bank {} - 90.0".format(target)
-    bank_id = braggtree._get_bank_id(bank_wksp_name)
+    bank_id = bragg_tree._get_bank_id(bank_wksp_name)
     assert int(bank_id) == target
 
-def test_get_bank_id_exception(qtbot, braggtree):
+def test_get_bank_id_exception(qtbot, bragg_tree):
     """Test for raised exception from a bad workspace name"""
     bad_ws = "Bank jkl 1 -- 90.0"
     with pytest.raises(BankRegexException) as e:
-        braggtree._get_bank_id(bad_ws)
+        bragg_tree._get_bank_id(bad_ws)
 
-def test_do_plot_ws_exception(qtbot, braggtree):
+def test_do_plot_ws_exception(qtbot, bragg_tree):
     """Test for raised exception from MainWindow==None"""
     with pytest.raises(NotImplementedError) as e:
-        braggtree.do_plot_ws()
+        bragg_tree.do_plot_ws()
