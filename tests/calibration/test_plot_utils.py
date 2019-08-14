7d4ebf41de039f6faf24da09420ac879ed557aac
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-08-14 10:24:16 -0400

    Add addie.calibration.plot_utils.gr_nr_plot tests

--- tests/calibration/test_plot_utils.py
+++ tests/calibration/test_plot_utils.py
@@ -6,7 +6,7 @@ import pytest
 import mantid.simpleapi as ms
 
 from tests import DATA_DIR, IMAGE_DIR
-from addie.calibration.plot_utils import plot_delta_d_ttheta
+from addie.calibration import plot_utils
 
 
 @pytest.fixture
@@ -24,16 +24,37 @@ def res_wksp():
     return ms.mtd['NOM_res']
 
 
-#test case without grouping workspace
+@pytest.fixture
+def diamond_nr():
+    nr_filename = os.path.join(DATA_DIR, 'diamond_nr.nxs')
+    ms.LoadNexusProcessed(Filename=nr_filename, OutputWorkspace='nr')
+    return ms.mtd['nr']
+
+
+@pytest.fixture
+def diamond_gr():
+    nr_filename = os.path.join(DATA_DIR, 'diamond_gr.nxs')
+    ms.LoadNexusProcessed(Filename=nr_filename, OutputWorkspace='gr')
+    return ms.mtd['gr']
+
+
 @pytest.mark.mpl_image_compare(baseline_dir=IMAGE_DIR,
                                filename="plot_delta_d_ttheta.png")
 def test_plot_delta_d_ttheta(res_wksp):
-    fig = plot_delta_d_ttheta('NOM_res')
+    fig = plot_utils.plot_delta_d_ttheta('NOM_res')
     return fig
 
 
 @pytest.mark.mpl_image_compare(baseline_dir=IMAGE_DIR,
                                filename="plot_delta_d_ttheta_with_group.png")
 def test_plot_delta_d_ttheta_with_group(res_wksp, grp_wksp):
-    fig = plot_delta_d_ttheta('NOM_res', group_workspace='NOM_group')
+    fig = plot_utils.plot_delta_d_ttheta('NOM_res',
+                                         group_workspace='NOM_group')
+    return fig
+
+
+@pytest.mark.mpl_image_compare(baseline_dir=IMAGE_DIR,
+                               filename="gr_nr_plot.png")
+def test_gr_nr_plot(diamond_gr, diamond_nr):
+    fig = plot_utils.plot_gr_nr('gr', 'nr', expected_n=[4, 16, 28])
     return fig
