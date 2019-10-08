b15df14e2b245dc03768a158d7d6c2e4a395f78a
McDonnell, Marshall T <mcdonnellmt@ornl.gov>
2019-10-08 12:26:26 -0400

    Re #283 - Fix script_to_run for NDabs

--- addie/processing/idl/run_ndabs.py
+++ addie/processing/idl/run_ndabs.py
@@ -15,18 +15,16 @@ class RunNDabs(object):
 
     def run(self):
         _str_list_sample_files = " ".join(self.list_sample_files)
-        _script_to_run = self.script_to_run + ' ' + str(self.parent.ui.run_ndabs_output_file_name.text()) + '.ndsum'
-        _script_to_run += ' ' + _str_list_sample_files
+        ui = self.parent.postprocessing_ui
+        _output_filename = ui.run_ndabs_output_file_name.text()
+        _script_to_run = "{} {}.ndsum {}".format(
+            self.script_to_run,
+            _output_filename,
+            _str_list_sample_files)
 
         JobStatusHandler(parent=self.parent,
                          script_to_run=_script_to_run,
                          job_name='NDabs')
-        # job_handler.launch(script_to_run=_script_to_run)
-
-        #_run_thread = self.parent._run_thread
-        #_run_thread.setup(script = _script_to_run)
-        # _run_thread.start()
 
         print("[LOG] executing:")
         print("[LOG] " + _script_to_run)
-        # os.system(_script_to_run)
