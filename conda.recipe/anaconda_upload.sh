5855bb95e081b88eac65f18824ddecf7cbf448da
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-09-10 13:30:43 -0400

    Debug CD conda build

--- conda.recipe/anaconda_upload.sh
+++ conda.recipe/anaconda_upload.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
   
-# Input
-if [[ $# -ne 1 ]]
-then
-  echo "Usage: anaconda_upload.sh <full package path>"
-  exit 1
-fi
 
-PKG_PATH=$1
+conda install conda-build conda-verify
+conda build . 
+
+export PKG_PATH=$(conda build . --output)
 PKG_FILE=$(basename ${PKG_PATH})
+echo ${PKG_FILE}
+echo ${PKG_PATH}
 
-echo "Uploading ${PKG_PATH} artifact..."
-anaconda -v -t ${CONDA_UPLOAD_TOKEN} upload ${PKG_PATH} --force
+#echo "Uploading ${PKG_PATH} artifact..."
+#anaconda -v -t ${CONDA_UPLOAD_TOKEN} upload ${PKG_PATH} --force
 
-echo "Successfully deployed ${PKG_FILE} to Anaconda.org."
+#echo "Successfully deployed ${PKG_FILE} to Anaconda.org."
